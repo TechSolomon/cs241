9ca01d6ccf9723cef8e162b8d2a78a7f26e24501
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-04-28 23:23:49 -0800

    refactor: final project code

--- project/bear-aware-smart-latch/bear-aware-smart-latch.ino
+++ project/bear-aware-smart-latch/bear-aware-smart-latch.ino
@@ -4,16 +4,16 @@
 
 #include <Servo.h>
 
-// Servo
+// Servo Motor
 Servo latch;
 const int servoPin = 9;
 int pos = 0;
 
-// Water Sensor
+// Water Sensor Data
 const int analogInPin = A0;
 int sensorValue = 0;
 
-// RGB LED (red, green, & blue pins)
+// RGB LED (red, green, & blue) Pins
 const char ledPins[3] = {2, 3, 4};
 int ledTime = 0;
 
@@ -21,9 +21,9 @@ int ledTime = 0;
 int red[4] = {500, 0, 0};
 int green[5] = {0, 500, 0};
 int blue[6] = {0, 0, 500};
-int low[7] = {0, 500, 0};
+int low[7] = {250, 500, 500};
 int medium[8] = {500, 500, 250};
-int high[9] = {500, 0, 0};
+int high[9] = {500, 250, 500};
 
 void getServoState() {
   // 0 degrees to 180 degrees.
@@ -39,8 +39,8 @@ void getServoState() {
   }
 }
 
+// Close from open position = 0 degrees to 45 degrees.
 void lock() {
-  // 0 degrees to 180 degrees.
   for (pos = 0; pos <= 45; pos += 1) {
     latch.write(pos);
     delay(15);
@@ -50,8 +50,8 @@ void lock() {
   showColor(denied);
 }
 
+// Open from closed position = 180 degrees to 0 degrees.
 void unlock() {
-  // 180 degrees to 0 degrees.
   for (pos = 45; pos >= 0; pos -= 1) {
     latch.write(pos);
     delay(15);
@@ -61,10 +61,11 @@ void unlock() {
   showColor(granted);
 }
 
+// Report current levels on the sensor.
 void getWaterLevel() {
   sensorValue = analogRead(analogInPin);
 
-  Serial.print("Water Level = " );
+  Serial.print("Water Level = ");
   Serial.println(sensorValue);
 
   delay(1000);
@@ -89,6 +90,7 @@ void showColor(const int *color) {
   delayMicroseconds(3000 - totalDelay);
 }
 
+// Smooth shift between custom RGB LED colors.
 void getCurrentColor() {
   int color[3] = {ledTime / 10, 0, 500 - ledTime / 10};
   showColor(color);
@@ -107,8 +109,8 @@ void setup() {
 
   for (char p : ledPins) pinMode(p, OUTPUT);
 
-  // Attaches the servo (pin 9) to the latch object.
-  latch.attach(9);
+  // Attaches the servo pin to the latch object.
+  latch.attach(servoPin);
 }
 
 String buffer;
@@ -117,6 +119,7 @@ void clearBuffer() {
   buffer = "";
 }
 
+// Takes project commands from user input.
 void instructions() {
   while (Serial.available()) {
     char c = Serial.read();
@@ -136,6 +139,7 @@ void instructions() {
           Serial.print("\nHelp Menu:\n");
           Serial.print("*: Clears buffer.\n");
           Serial.print("ok: Checks the Arduino status.\n");
+          Serial.print("debug: Checks the Servo & LED status.\n");
           Serial.print("water: Checks the water sensor.\n");
           Serial.print("i: Locks the Smart Latch.\n");
           Serial.print("o: Unlocks the Smart Latch.\n");
@@ -149,32 +153,41 @@ void instructions() {
           clearBuffer();
         }
 
+      case 'g':
+        // Debugs the Servo Motor (full rotation) & RGB LED (color shifting).
+        if (buffer == "debug") {
+          getServoState();
+          getCurrentColor();
+          clearBuffer();
+        }
+
       case 'r':
-        // Check the water level.
+        // Check the current water level.
         if (buffer = "water") {
 
-          for (int checkDepth = 0; checkDepth < 20; checkDepth++) {
+          for (int checkDepth = 0; checkDepth < 10; checkDepth++) {
             getWaterLevel();
 
             if (sensorValue = 0) {
               Serial.println("[WATER EMPTY]");
+              showColor(red);
             }
 
             else if (sensorValue > 0 && sensorValue <= 300) {
-              Serial.println("[LOW LEVEL]");
+              Serial.println("[LOW WATER LEVEL]");
+              showColor(low);
             }
 
             else if (sensorValue > 300 && sensorValue <= 500) {
-              Serial.println("[MEDIUM LEVEL]");
+              Serial.println("[MEDIUM WATER LEVEL]");
+              showColor(medium);
             }
 
             else if (sensorValue > 500) {
-              Serial.println("[HIGH LEVEL]");
+              Serial.println("[HIGH WATER LEVEL]");
+              showColor(high);
             }
           }
-
-          // TODO: Change LED color depending on the water depth.
-          // getCurrentColor();
 
           clearBuffer();
         }
@@ -202,10 +215,6 @@ void instructions() {
   }
 }
 
-// TODO: Fix flickering RGB LED with water inputs.
 void loop() {
   instructions();
-  //  getServoState();
-  //  getWaterLevel();
-  //  getCurrentColor();
 }
